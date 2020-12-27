bd60243f08b2776e48a6060dfdb2cedac43b2c5d
DemarioDouce <demario.douce567@gmail.com>
2020-12-26 22:25:39 -0500

    used mongoDB create options

--- mongodb.js
+++ mongodb.js
@@ -17,10 +17,51 @@ mongoClient.connect(
     //database ref
     const db = client.db(databaseName);
 
-    //insert doc
-    db.collection("users").insertOne({
-      name: "Demario",
-      age: 22,
-    });
+    //insert one doc
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Demario",
+    //     age: 22,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert user");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    //insert many doc
+    db.collection("users").insertMany(
+      [
+        {
+          name: "Jen",
+          age: 26,
+        },
+        { name: "Blake", age: 25 },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert docs");
+        }
+
+        console.log(result.ops);
+      }
+    );
+
+    db.collection("tasks").insertMany(
+      [
+        { description: "Code more", completed: true },
+        { description: "Push to github", completed: true },
+        { description: "Graduate in 2099", completed: false },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert docs");
+        }
+
+        console.log(result.ops);
+      }
+    );
   }
 );
