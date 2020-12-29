4bea3aac9741b3f521ea3382191044fa2da610db
DemarioDouce <demario.douce567@gmail.com>
2020-12-28 21:39:27 -0500

    more mongoose stuff added

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,6 @@
 //npm module
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/TaskAppAPI", {
   useNewUrlParser: true,
@@ -7,29 +8,51 @@ mongoose.connect("mongodb://127.0.0.1:27017/TaskAppAPI", {
 });
 
 const User = mongoose.model("User", {
-  name: { type: String },
-  age: { type: Number },
+  name: { type: String, required: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw console.log("Email is invalid");
+      }
+    },
+    trim: true,
+    lowercase: true,
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < -1) {
+        throw new Error("Age must be a positive number.");
+      }
+    },
+  },
 });
 
-// const me = new User({ name: "Demario", age: 22 })
-//   .save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-const Task = mongoose.model("Task", {
-  description: { type: String },
-  completed: { type: Boolean },
-});
-
-const newTask = new Task({ description: "Fix weather bug", completed: false })
+const newUser = new User({
+  name: "Demario",
+  email: "ddouce@gmail.com",
+  age: 22,
+})
   .save()
   .then(() => {
-    console.log(newTask);
+    console.log(newUser);
   })
   .catch((error) => {
     console.log(error);
   });
+
+// const Task = mongoose.model("Task", {
+//   description: { type: String },
+//   completed: { type: Boolean },
+// });
+
+// const newTask = new Task({ description: "Fix weather bug", completed: false })
+//   .save()
+//   .then(() => {
+//     console.log(newTask);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
