bd23d7a90c0286a7432d6a3ddc616bd19d0ceb2c
DemarioDouce <demario.douce567@gmail.com>
2020-12-31 12:32:10 -0500

    added get method

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,8 +5,3 @@ mongoose.connect("mongodb://127.0.0.1:27017/TaskAppAPI", {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
-
-const Task = mongoose.model("Task", {
-  description: { type: String, required: true, trim: true },
-  completed: { type: Boolean },
-});
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const user = require("./models/user");
+const task = require("./models/task");
 
 const app = express();
 
@@ -8,12 +9,55 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+//users
+
+app.get("/users", (req, res) => {
+  user
+    .find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((error) => {
+      res.sendStatus(500).send(error);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  let userId = req.params.id;
+
+  user
+    .findById(userId)
+    .then((users) => {
+      if (!users) {
+        res.sendStatus(404).send();
+      } else {
+        res.send(users);
+      }
+    })
+    .catch((error) => {
+      res.sendStatus(500).send(error);
+    });
+});
+
 app.post("/users", (req, res) => {
   let newUser = new user(req.body);
   newUser
     .save()
     .then(() => {
-      res.send(newUser);
+      res.sendStatus(201).send(newUser);
+    })
+    .catch((error) => {
+      res.sendStatus(400).send(error);
+    });
+});
+
+//tasks
+app.post("/tasks", (req, res) => {
+  let newTask = new task(req.body);
+  newTask
+    .save()
+    .then(() => {
+      res.sendStatus(201).send(newTask);
     })
     .catch((error) => {
       res.sendStatus(400).send(error);
