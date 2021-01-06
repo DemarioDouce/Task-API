6b24c3b72e5c47feea9baf1baf4cad0ec20e1e82
DemarioDouce <demario.douce567@gmail.com>
2021-01-05 19:36:51 -0500

    Explored async await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -3,12 +3,26 @@ const task = require("../src/models/task");
 
 //
 
-task
-  .findByIdAndDelete({ _id: "5fea53c920080716717e6c29" })
-  .then((foundTask) => {
-    console.log(foundTask);
-    return task.countDocuments({ completed: true });
-  })
+// task
+//   .findByIdAndDelete({ _id: "5fea53c920080716717e6c29" })
+//   .then((foundTask) => {
+//     console.log(foundTask);
+//     return task.countDocuments({ completed: true });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const findAndDelet = async (id) => {
+  const foundTask = await task.findByIdAndDelete(id);
+  const count = await task.countDocuments(id);
+  return count;
+};
+
+findAndDelet("5feabfd009abb02cb0e7e2ec")
   .then((result) => {
     console.log(result);
   })
